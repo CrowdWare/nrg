795fc320558bc091b92cd26a9d96830a5e75de61
Artanidos <artanidos@gmail.com>
2023-06-01 18:53:38 +0200

    Android app added, website moved

---
+++ app/src/main/java/at/crowdware/nrg/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package at.crowdware.nrg.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryDark = Color(0xFFa0ce4d)
+val SecondaryDark = Color(0xFFa0ce4d)
+val TertiaryDark = Color(0xFFa0ce4d)
+val BackgroundDark = Color(0xFF1a1a1a)
+val SurfaceDark = Color(0xFF1a1a1a)
+val OnPrimaryDark = Color(0xFF1a1a1a)
+val OnBackgroundDark = Color(0xFFCCCCCC)
+val OnSurfaceDark = Color(0xFFCCCCCC)
+
+val PrimaryLight = Color(0xFFa0ce4d)
+val SecondaryLight = Color(0xFFa0ce4d)
+val TertiaryLight = Color(0xFFa0ce4d)
+val BackgroundLight = Color(0xFF1a1a1a)
+val SurfaceLight = Color(0xFF1a1a1a)
+val OnPrimaryLight = Color(0xFF1a1a1a)
+val OnBackgroundLight = Color(0xFFCCCCCC)
+val OnSurfaceLight = Color(0xFFCCCCCC)
