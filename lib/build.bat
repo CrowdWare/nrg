117d3880130125cb9452070e6af52475bf32323e
Artanidos <artanidos@gmail.com>
2023-06-08 07:01:33 +0200

    upd

--- lib/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set GOOS=android
-set CGO_ENABLED=1
-set ANDROID_NDK_HOME=D:/Android/android-sdk/ndk/25.2.9519653
-
-REM Build the AAR file for all architectures
-gomobile bind -androidapi 28 -target=android -o libnrg.aar nrg
-copy libnrg.aar E:\SourceCode\nrg\app\libs
--- lib/lib.go
+++
@@ -1,9 +0,0 @@
-package nrg
-
-func Add(a, b int) int {
-	return a + b
-}
-
-func MyFunction() string {
-	return "Hello from MyFunction!"
-}
